a21435ee0fe9024eb8e3fd08e955c02260c35c77
robertoherrera <roberto.herrera@portaldata.cl>
2021-10-18 22:07:40 -0300

    limpiando

--- datab/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "papa firtaaa";
-function retornarConexion() {
-  $con=mysqli_connect("localhost","root","","Clinica");
-  return $con;
-}
-?>
--- datab/usuario.ms.ts
+++
@@ -1,3 +0,0 @@
-export class UsuarioMs {
-    constructor(public nombre: string, public apellido: string, public correo: string, public numero: string, public clave: string) {}
-}
